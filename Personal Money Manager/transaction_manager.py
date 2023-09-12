2b9501f4f2583411959d68a3f7ccced35d2dba95
Lance Barry Babiano <113407178+codepap1@users.noreply.github.com>
2023-09-13 00:09:05 +0800

    Update transaction_manager.py

--- Personal Money Manager/transaction_manager.py
+++ Personal Money Manager/transaction_manager.py
@@ -1,83 +1,96 @@
 import datetime
 
 
-# Withdraw money
-def withdraw(username):
-    amount = float(input("Enter the amount to withdraw: "))
+class TransactionManager:
+    def __init__(self):
+        self.filename = "database.txt"
 
-    # Update account balance in the database
-    updated_lines = []
-    with open("database.txt", "r") as file:
-        for line in file:
-            account = line.strip().split(",")
-            if account[0] == username:
-                if amount > 0 and amount <= float(account[2]):
+    def get_account_info(self, username):
+        with open(self.filename, "r") as file:
+            for line in file:
+                account = line.strip().split(",")
+                if account[0] == username:
+                    return account
+        return None
+
+    def update_account(self, updated_account):
+        updated_lines = []
+        with open(self.filename, "r") as file:
+            for line in file:
+                account = line.strip().split(",")
+                if account[0] == updated_account[0]:
+                    updated_lines.append(",".join(updated_account))
+                else:
+                    updated_lines.append(line)
+
+        with open(self.filename, "w") as file:
+            file.writelines(updated_lines)
+
+    def deposit(self, username):
+        while True:
+            try:
+                amount = float(input("Enter the amount to deposit: "))
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+                continue
+
+            if amount <= 0:
+                print("Invalid amount. Amount must be greater than 0.")
+                continue
+
+            account = self.get_account_info(username)
+            if account:
+                account[2] = str(float(account[2]) + amount)
+                account.append(f"{datetime.datetime.now()},Deposit,{amount}")
+                self.update_account(account)
+                print(f"Deposited ${amount}. New balance: ${account[2]}")
+                break
+
+    def withdraw(self, username):
+        while True:
+            try:
+                amount = float(input("Enter the amount to withdraw: "))
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+                continue
+
+            if amount <= 0:
+                print("Invalid amount. Amount must be greater than 0.")
+                continue
+
+            account = self.get_account_info(username)
+            if account:
+                if float(account[2]) >= amount:
                     account[2] = str(float(account[2]) - amount)
                     account.append(f"{datetime.datetime.now()},Withdraw,{amount}")
+                    self.update_account(account)
+                    print(f"Withdrew ${amount}. New balance: ${account[2]}")
+                    break
                 else:
-                    print("Invalid amount or insufficient balance.")
-            updated_lines.append(",".join(account) + "\n")
+                    print("Insufficient balance.")
 
-    # Rewrite the database with updated account details
-    with open("database.txt", "w") as file:
-        file.writelines(updated_lines)
-
-
-# Deposit money
-def deposit(username):
-    amount = float(input("Enter the amount to deposit: "))
-
-    # Update account balance in the database
-    updated_lines = []
-    with open("database.txt", "r") as file:
-        for line in file:
-            account = line.strip().split(",")
-            if account[0] == username:
-                if amount > 0:
-                    account[2] = str(float(account[2]) + amount)
-                    account.append(f"{datetime.datetime.now()},Deposit,{amount}")
-                else:
-                    print("Invalid amount.")
-            updated_lines.append(",".join(account) + "\n")
-
-    # Rewrite the database with updated account details
-    with open("database.txt", "w") as file:
-        file.writelines(updated_lines)
-
-
-# Check balance
-def check_balance(username):
-    with open("database.txt", "r") as file:
-        for line in file:
-            account = line.strip().split(",")
-            if account[0] == username:
-                balance = float(account[2])
-                print(f"Your current balance is: ${balance}")
-                break
-
-
-# Check transaction history
-def check_transaction_history(username):
-    with open("database.txt", "r") as file:
-        for line in file:
-            account = line.strip().split(",")
-            if account[0] == username:
-                transactions = account[3:]
-                if len(transactions) == 0:
-                    print("No transactions found.")
-                else:
-                    print("Transaction History:")
-                    for transaction in transactions:
-                        print(transaction)
-                break
-
-
-# Generate overall report
-def generate_report():
-    print("Overall Report:")
-    with open("database.txt", "r") as file:
-        for line in file:
-            account = line.strip().split(",")
-            username = account[0]
+    def check_balance(self, username):
+        account = self.get_account_info(username)
+        if account:
             balance = float(account[2])
-            print(f"Username: {username}, Balance: ${balance}")
+            print(f"Your current balance is: ${balance}")
+
+    def check_transaction_history(self, username):
+        account = self.get_account_info(username)
+        if account:
+            transactions = account[3:]
+            if len(transactions) == 0:
+                print("No transactions found.")
+            else:
+                print("Transaction History:")
+                for transaction in transactions:
+                    print(transaction)
+
+    def generate_report(self):
+        print("Overall Report:")
+        with open(self.filename, "r") as file:
+            for line in file:
+                account = line.strip().split(",")
+                username = account[0]
+                balance = float(account[2])
+                print(f"Username: {username}, Balance: ${balance}")
