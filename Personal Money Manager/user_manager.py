b3e7225377a8645cc043288c9e3dd1e7a8d94bcf
Lance Barry Babiano <113407178+codepap1@users.noreply.github.com>
2023-09-13 00:08:45 +0800

    Update user_manager.py

--- Personal Money Manager/user_manager.py
+++ Personal Money Manager/user_manager.py
@@ -1,29 +1,32 @@
-# Create account
-def create_account():
-    username = input("Enter a username: ")
-    password = input("Enter a password: ")
-    balance = float(input("Enter your initial balance: "))
+class UserManager:
+    def __init__(self):
+        self.filename = "database.txt"
 
-    # Save account details to the database
-    with open("database.txt", "a") as file:
-        file.write(f"{username},{password},{balance}\n")
+    def create_account(self):
+        username = input("Enter a username: ")
+        password = input("Enter a password: ")
+        balance = float(input("Enter your initial balance: "))
 
-    print("Account created successfully!")
+        with open(self.filename, "a") as file:
+            file.write(f"{username},{password},{balance}\n")
 
+        print("\n====================================")
+        print("Account created successfully!")
 
-# User authentication and login
-def login():
-    print("\n====Login====")
-    username = input("Username: ")
-    password = input("Password: ")
+    def login(self):
+        while True:
+            print("\n======== Login ========")
+            username = input("Username: ")
+            password = input("Password: ")
 
-    # Check if account exists in the database
-    with open("database.txt", "r") as file:
-        for line in file:
-            account = line.strip().split(",")
-            if account[0] == username and account[1] == password:
-                print("Login successful!")
-                return username
+            # Check if account exists in the database
+            with open(self.filename, "r") as file:
+                for line in file:
+                    account = line.strip().split(",")
+                    if account[0] == username and account[1] == password:
+                        print("\n====================================")
+                        print("Login successful!")
+                        return username
 
-    print("Invalid username or password.")
-    return None
+            print("\n====================================")
+            print("Invalid username or password. Please try again.")
